d460cbb3842ee1690963ebc3d67fb89da58ff59c
Ryoshi <fischerklon3@gmail.com>
2023-06-13 22:08:46 +0200

    Configuration Files

---
+++ src/main/java/com/Ryoshi/DatabaseProgram/DatabaseProgramApplication.java
@@ -0,0 +1,13 @@
+package com.Ryoshi.DatabaseProgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseProgramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Ryoshi/DatabaseProgram/DatabaseProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ryoshi.DatabaseProgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
