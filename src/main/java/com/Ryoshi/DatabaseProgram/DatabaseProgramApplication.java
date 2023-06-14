fc63dc67fad3950000c11e5d3eb85b9f70470bba
Fischer <dominik.fischer@cewe.de>
2023-06-14 10:10:53 +0200

    Testing Connectivity between Java HTML CSS and JavaScript

--- src/main/java/com/Ryoshi/DatabaseProgram/DatabaseProgramApplication.java
+++ src/main/java/com/Ryoshi/DatabaseProgram/DatabaseProgramApplication.java
@@ -2,12 +2,21 @@ package com.Ryoshi.DatabaseProgram;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@Controller
 public class DatabaseProgramApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DatabaseProgramApplication.class, args);
+	}
+
+	@GetMapping("/notIndex")
+	public String redirectToNotIndex(){
+		return "redirect:notIndex.html";
 	}
 
 }
---
+++ src/main/resources/static/script.js
@@ -0,0 +1,5 @@
+let current = 0;
+function testButton(n){
+    current+=n;
+    console.log(current);
+}
